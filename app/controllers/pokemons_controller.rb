74a9c02ce61361447616824e1889f2ea28cd056e
GuzelGokhan <gokhan@medispring.be>
2020-07-23 20:35:14 +0200

    correction of last commit

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,6 @@
 class PokemonsController < ApplicationController
   before_action :set_pokemon, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only:[:index]
+  skip_before_action :authenticate_user!, only:[:index, :show]
 
   def index
     @user = current_user
