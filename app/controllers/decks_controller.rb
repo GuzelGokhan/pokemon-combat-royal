15ca46f993bbd48c0a4c152cd59a3cc2bc04ef0e
GuzelGokhan <gokhan@medispring.be>
2020-07-24 11:23:55 +0200

    decks model controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,36 @@
 class DecksController < ApplicationController
+before_action :set_pokemon , only: [:new,:create]
+
+  def show
+    @deck = Deck.find(params[:id])
+  end
+  
+  def new
+    @deck = Deck.new
+    @deck.pokemon = @pokemon
+  end
+
+  def create
+    @deck = Deck.new(params_deck)
+    @deck.pokemon = @pokemon 
+    @deck.user = current_user
+    if @deck.save
+      flash[:success] = "Deck successfully created"
+      redirect_to @deck
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:pokemon_id])
+  end
+
+  def params_deck
+    params.require(:deck).permit(:start,:end, :user_id, :pokemon_id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pokemons
+  resources :pokemons do 
+    resources :decks , only: [:new, :create]
+  end
+
+  resources :decks, only: [:show, :destroy, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
