e0c565dfcb94f778f5167248d456658367008ebf
GuzelGokhan <gokhan@medispring.be>
2020-07-25 15:50:20 +0200

    battle duel

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,10 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const buttons = document.querySelectorAll(".clickable");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("active");
+  });
+});
